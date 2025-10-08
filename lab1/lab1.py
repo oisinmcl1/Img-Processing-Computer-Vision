a3a7e26857da01800979c8d63221bf168561ddd6
oisinmcl1 <omclaughlin99@gmail.com>
2025-10-08 17:21:25 +0100

    lab 1 section 2

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -30,3 +30,27 @@ plt.show()
 equ = cv.equalizeHist(img)
 res = np.hstack((img, equ)) # Put images side by side
 cv.imwrite('res.png', res)  # Save the result
+
+
+### SECTION 2 ###
+
+# Different types of thresholding
+ret,thresh1 = cv.threshold(img,127,255,cv.THRESH_BINARY) # Simple binary thresholding - dark pixels become black, bright pixels become white
+ret,thresh2 = cv.threshold(img,127,255,cv.THRESH_BINARY_INV) # Inverse binary thresholding - dark pixels become white, bright pixels become black
+ret,thresh3 = cv.threshold(img,127,255,cv.THRESH_TRUNC) # Truncate thresholding - bright pixels become the threshold value, dark pixels remain unchanged
+ret,thresh4 = cv.threshold(img,127,255,cv.THRESH_TOZERO) # To zero thresholding - dark pixels become black, bright pixels remain unchanged
+ret,thresh5 = cv.threshold(img,127,255,cv.THRESH_TOZERO_INV) # Inverse to zero thresholding - dark pixels remain unchanged, bright pixels become black
+
+# titles for the plots
+titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+
+# images to be plotted
+images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+
+# Plot all the images
+for i in range(6):
+    plt.subplot(2, 3, i + 1), plt.imshow(images[i], 'gray', vmin=0, vmax=255)
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+
+plt.show()
