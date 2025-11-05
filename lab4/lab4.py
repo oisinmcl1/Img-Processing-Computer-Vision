4ae292336c6c7ded92ba729889486425b664c7a4
oisinmcl1 <omclaughlin99@gmail.com>
2025-11-05 17:06:12 +0000

    task 2

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 IMG_PATH = "flowers.jpg"
 img = cv.imread(IMG_PATH)
 
+# =================== TASK 1 ===================
+
 # Convert to grayscale and apply Gaussian blur
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 blur = cv.GaussianBlur(gray, (5, 5), 1.0)
@@ -32,3 +34,50 @@ plt.title("Canny 100/200");
 plt.axis("off")
 
 plt.show()
+
+# ==============================================
+
+# =================== TASK 2 ===================
+
+# Convert to float32 for Harris corner detection
+gray_f = np.float32(gray)
+
+# Harris Corner Detection
+harris = cv.cornerHarris(gray_f, blockSize=2, ksize=3, k=0.04)
+# Dilate to mark the corners
+harris = cv.dilate(harris, None)
+harris_img = img.copy()
+# Mark corners in the image in red
+harris_img[harris > 0.01 * harris.max()] = [0, 0, 255]
+
+# Shi–Tomasi (Tomasi–Kanade)
+shi_img = img.copy()
+# Marks corners using Shi-Tomasi method
+pts = cv.goodFeaturesToTrack(gray, maxCorners=150, qualityLevel=0.01, minDistance=8)
+
+# If corners are found, draw them
+if pts is not None:
+    pts = np.int32(pts)
+    # For each corner point, draw a circle on the image in green
+    for p in pts:
+        x, y = p.ravel()
+        cv.circle(shi_img, (x, y), 3, (0, 255, 0), -1)
+
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 3, 1);
+plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB));
+plt.title("Original");
+
+plt.axis("off")
+plt.subplot(1, 3, 2);
+plt.imshow(cv.cvtColor(harris_img, cv.COLOR_BGR2RGB));
+plt.title("Harris");
+plt.axis("off")
+
+plt.subplot(1, 3, 3);
+plt.imshow(cv.cvtColor(shi_img, cv.COLOR_BGR2RGB));
+plt.title("Shi–Tomasi");
+plt.axis("off")
+plt.show()
+
+# ==============================================
